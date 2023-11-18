9a838fe6ec3552543d0d427bd38ca76000b688d0
nasneas <joananeasamoraga@gmail.com>
2023-11-18 17:50:01 +0100

    v1.0.7 - rediseño interfaz

--- index.php
+++ index.php
@@ -14,96 +14,50 @@
 <!-- Nav DINAMICO -->
 <?php require "dynamo/nav-dinamico.php"; ?>
     <main>
+        <section class="carroussel">
+            <button class="anterior">Anterior</button>
+            <img class="carroussel-img" src="media/prueba.jpg">
+            <button class="siguiente">Siguiente</button>
+            <ul class="carroussel-puntos">
+                <li class="activo"></li>
+                <li></li>
+                <li></li>
+                <li></li>
+                <li></li>
+            </ul>
+        </section>
+        
         <section class="frame">
-            <div class="header-libros">
-                <span>Llistat llibres <span class="fuenteH1" style="font-size:18px; text-decoration:underline;"><?php echo $nomBiblioteca; ?></span></span>
-            <form style="float: right;" method="post" action=""> <!--Cerca de llibres-->
-                <label>Cerca</label>
-                <input name="inputCerca" class="enFocus" type="text" value="<?php
-                    if ($_SERVER["REQUEST_METHOD"] == "POST") {                
-                        // Obtener el valor del campo de entrada de texto
-                        $inputCerca = $_POST["inputCerca"];
-                        #$inputCerca = $conexion->real_escape_string($inputCerca);
-                        $sql = "SELECT * FROM llibres
-                        WHERE nomLlibre LIKE '%".$inputCerca."%';";
+            <div class="header-section">
+                <span style="float: left;">Cerca per:</span>
+                <a href="#"><i>Clic per a veure tots.</i></a>
+            </div>
+            <div class="categorias">
+                <button>Categoría 1</button>
+                <button>Categoría 2</button>
+                <button>Categoría 3</button>
+            </div>
 
-                        // Ejecutar la consulta
-                        $result = $conexion->query($sql);
-                        
-                        // Verificar si hay resultados
-                        if ($result->num_rows > 0) {
-                            // Recorrer los resultados
-                            while ($row = $result->fetch_assoc()) {
-                                echo $row["nomLlibre"];
-                            }
-                        } else {
-                            echo "No hi han llibres disponibles.";
-                        }
-                    }
-                ?>">
-                
-                <button class="boto-cerca" type="submit"><img src="./media/sistema/buscar.png" alt="Enviar" width="20" height="20"></button>                
-            </form>
-            </div><br>
-            <!--dinamico-->
-            <?php
-                $sql = "SELECT llibres.idLlibre, llibres.nomLlibre, llibres.descripcio  FROM llibres";
+            <div class="topicos">
+                <button>Tópico 1</button>
+                <button>Tópico 2</button>
+                <button>Tópico 3</button>
+                <button>Tópico con un nombre largo</button>
+                <button>Otro Tópico</button>
+            </div>
+        </section>
 
-                // Ejecutar la consulta
-                $result = $conexion->query($sql);
-                
-                // Verificar si hay resultados
-                if ($result->num_rows > 0) {
-                    // Recorrer los resultados
-                    while ($row = $result->fetch_assoc()) {
-                        // Uso el print para las etiquetas html y echo para el php (meramente human-friendly)
-                        $idLlibre = $row["idLlibre"];
-                        $descripcioCurta = strlen($row['descripcio']) > 70 ? (substr($row['descripcio'], 0, 70) . "...") : $row['descripcio'];
-                        print("<article class='libros'>");
-                        # Título
-                        print("<h3 class='atrib'>");
-                        echo  $row["nomLlibre"];
-                        print("</h3>");
-                        # Imagen (src dinámico)
-                        print("<img class='atrib' src='./media/sistema/pruebas.jpg' width='100' height='100' alt='libro'>");
-                        # Salto de línea (br)
-                        print("<br>");
-                        #Valoración
-                        print("<span class='atrib'>");
-                        echo "estrellas";
-                        print("</span>");
-                        # Descripción
-                        print("<p class='atrib descrip'>");
-                        echo $descripcioCurta;
-                        print("</p>");
-                        # Saber más...
-                        print("<button class='botonUniversal botonLibros'><a href='libro.php?id=".$idLlibre."'>Llegir més</a></button>");
-                        print("</article>");
-                        # Fin Libro
-                    }
-                } else {
-                    echo "No hi han llibres disponibles.";
-                }
-                
-                ######## ¡¡¡¡¡¡¡DEBUGGING!!!!!!!#######
-                //        (quitar en produccion)
-                $sql = "SELECT llibres.nomLlibre, llibres.idLlibre FROM llibres WHERE idLlibre = 1";
-                $resultado = $conexion->query($sql);
-
-                if ($resultado && $fila = $resultado->fetch_assoc()) {
-                    echo "<p>DEBUG: " . $fila['nomLlibre'] . " id: ". $fila['idLlibre'];
-                } else {
-                    echo "<p>No se encontraron registros para el ID 1.</p>";
-                }
-                ######## ¡¡¡¡¡¡¡DEBUGGING!!!!!!!#######
-            ?>
-
-                <!-- <h3 class="atrib">Titulo</h3>
-                <img class="atrib" src="./media/sistema/pruebas.jpg" width="100" height="100" alt="libro">
-                <br>
-                <span class="atrib">estrellas</span>
-                <p class="atrib">descripion breve</p> -->
-            
+        <section class="frame">
+            <div class="header-section">
+                <span s="float: left;">Enquestes</span>
+            </div>
+            <ul>
+                <li>Opció 1</li>
+                <li>Opció 1</li>
+                <li>Opció 1</li>
+                <li>Opció 1</li>
+                <li>Opció 1</li>
+            </ul>
         </section>
     </main>
 
